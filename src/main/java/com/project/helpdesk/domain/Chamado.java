b560436d991f4b0d7006ed7ee744090d0be0e805
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-10-23 16:52:34 -0300

    Criação das classes de domínio

---
+++ src/main/java/com/project/helpdesk/domain/Chamado.java
@@ -0,0 +1,36 @@
+package com.project.helpdesk.domain;
+
+import com.project.helpdesk.domain.enums.Prioridade;
+import com.project.helpdesk.domain.enums.Status;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
+@Data
+@EqualsAndHashCode
+public class Chamado {
+
+    private Integer id;
+    private LocalDate dataAbertura = LocalDate.now();
+    private LocalDate dataFechamento;
+    private Prioridade prioridade;
+    private Status status;
+    private String titulo;
+    private String observacoes;
+
+    private Tecnico tecnico;
+    private Cliente cliente;
+
+    public Chamado() {
+    }
+
+    public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.prioridade = prioridade;
+        this.status = status;
+        this.titulo = titulo;
+        this.observacoes = observacoes;
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+}
