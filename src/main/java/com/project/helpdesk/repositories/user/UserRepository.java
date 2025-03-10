4c58832f9bf9908e017ab57911a003c7527cc40e
HenriqueV01 <henrique.unibratec01@gmail.com>
2025-03-10 11:56:55 -0300

    Implementação Spring Security.

---
+++ src/main/java/com/project/helpdesk/repositories/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.helpdesk.repositories.user;
+
+import com.project.helpdesk.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+
+}
